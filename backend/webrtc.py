dd1b314a39db033aecb0cf1822a2003e811281bb
Yousif <yousef2000khalid@gmail.com>
2020-07-11 19:18:36 +1000

    structure for webrtc side of the backend

--- backend/webrtc.py
+++ backend/webrtc.py
@@ -1,6 +1,6 @@
 from application import app
 
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send, emit
 from aiortc import RTCPeerConnection, RTCDataChannel, \
                    RTCIceCandidate, RTCSessionDescription
 
@@ -8,21 +8,30 @@ sio = SocketIO(app)
 
 WebRTCConnections = {}
 
+
+
 @sio.on("establish", namespace='/webrtc')
 async def establish(sid, environ):
-    WebRTCConnections = 
+    #create a webrtc connection
+    WebRTCConnections[sid] = RTCPeerConnection()
+    sdpOffer = WebRTCConnections[sid].createOffer()
+    emit("offer", sdpOffer, namespace='/webrtc',
+    callback=lambda: WebRTCConnections[sid].setLocalDescription(sdpOffer))
+
 
 @sio.on("answer", namespace='/webrtc')
-async def send_offer():
-    pass
+async def handle_answer(sidSrc, sidDest, answer):
+    #emit an offer
+    sio.emit("answer", answer, room=sidDest)
+    
 
 @sio.on("offer", namespace='/webrtc')
-async def send_answer():
-    pass
+async def handle_offer(sidSrc, sidDest, offer):
+    sio.emit("offer", offer, room=sidDest)
 
 @sio.on("icecandidate", namespace='/webrtc')
-async def add_icecandidate():
-    pass
+async def add_icecandidate(sidSrc, sidDest, icecandidate):
+    sio.emit("icecandidate", icecandidate, room=sidDest)
 
 @sio.on("disconnect")
 async def disconnect():
