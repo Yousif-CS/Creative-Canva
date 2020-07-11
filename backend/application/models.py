4fcac88fefd7787d946a9ed9181d8d430bb51ef9
Yousif <yousef2000khalid@gmail.com>
2020-07-11 20:13:48 +1000

    fixing errors in MongoEngine's declarations

--- backend/application/models.py
+++ backend/application/models.py
@@ -2,30 +2,30 @@ import flask
 from application import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
-# class User(db.Document):
-#     user_id     =   db.IntField( unique=True )
-#     first_name  =   db.StringField( max_length=20 )
-#     last_name   =   db.StringField( max_length=20 )
-#     email       =   db.StringField( max_length=30, unique=True )
-#     password    =   db.StringField( max_length=20)
-#     available   =   db.BooleanField( default=True )
+class User(db.Document):
+    user_id     =   db.IntField( unique=True )
+    first_name  =   db.StringField( max_length=20 )
+    last_name   =   db.StringField( max_length=20 )
+    email       =   db.StringField( max_length=30, unique=True )
+    password    =   db.StringField( max_length=20)
+    available   =   db.BooleanField( default=True )
     
-#     def set_password(self, password):
-#         self.password = generate_password_hash(password)
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
 
-#     def get_password(self, password):
-#         return check_password_hash(self.password, password)    
+    def get_password(self, password):
+        return check_password_hash(self.password, password)    
 
-# class Family(db.Document):
+#class Family(db.Document):
     
 
-# class Course(db.Document):
-#     courseID   =   db.StringField( max_length=10, unique=True )
-#     title       =   db.StringField( max_length=100 )
-#     description =   db.StringField( max_length=255 )
-#     credits     =   db.IntField()
-#     term        =   db.StringField( max_length=25 )
+class Course(db.Document):
+    courseID   =   db.StringField( max_length=10, unique=True )
+    title       =   db.StringField( max_length=100 )
+    description =   db.StringField( max_length=255 )
+    credits     =   db.IntField()
+    term        =   db.StringField( max_length=25 )
 
-# class Enrollment(db.Document):
-#     user_id     =   db.IntField()
-#     courseID    =   db.StringField( max_length=10 )
+class Enrollment(db.Document):
+    user_id     =   db.IntField()
+    courseID    =   db.StringField( max_length=10 )
